486312ebc9d1fb05f3234be88c64953f0371889c
John Koh <me@johnkoh.net>
2017-07-29 23:10:48 -0400

    add keng

--- keyboard/hhkb/keymap_keng.c
+++ keyboard/hhkb/keymap_keng.c
@@ -1,5 +1,5 @@
 /*
- * John's personal keyset
+ * HHKB Layout
  */
 #include "keymap_common.h"
 
@@ -24,9 +24,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      */
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,FN0,RGHT,    \
-                LALT,LGUI,          SPC,                LGUI,LEFT),
+           LSFT,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
+           LCTL,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RALT,FN0,             \
+                LALT,LGUI,          SPC,                LEFT,RGHT),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -34,17 +34,17 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
      * |-----------------------------------------------------------|
-     * |      |MUTE|VOLD|VOLU|MPLY|MNXT|  *|  /|Hom|PgU|Lef|Rig|Enter   |
+     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
      * |-----------------------------------------------------------|
      * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
      * `-----------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-    KEYMAP(POWER, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,F12,INS,TRNS,   \
-           CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, DEL,      \
-           TRNS,MUTE,VOLD,VOLU,MPLY,MNXT,PAST,PSLS,TRNS,HOME,PGUP,UP,PENT,              \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
+    KEYMAP(PWR, SLCK,  PAUS,  VOLD, VOLU,  MPLY,  MNXT,  MPRV,  MPLY,  MNXT,  MUTE,VOLD, VOLU, INS, DEL,   \
+           CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, TRNS, TRNS, BSPC,      \
+           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,TRNS,TRNS,TRNS,TRNS,UP,PENT,            \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,DOWN,TRNS,TRNS,            \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 };
 
